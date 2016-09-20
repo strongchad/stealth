0739cb994091518487238889679ca51d337e7079
strongchad <einarstockan@gmail.com>
2016-09-20 18:08:30 +0100

    Overhaul
    Complete overhaul. Now it doesn't suck. Brightness of a tile taken out
    of datatype and into functions

--- Light.hs
+++ Light.hs
@@ -1,63 +1,32 @@
-import Data.Foldable (toList)
-import Data.Sequence as Sequence
-
 type Coordinate = (Int,Int)
 
-data LightLevel = Drk | Dim | Brt deriving (Eq, Ord)
-
-data Tile = Tile LightLevel -- this datatype stores information about a tile; not about its contents
-
-type Grid = Seq (Seq Tile)
-
-data Direction = North | East | South | West deriving Eq
-
-data LightSource = SquareLight Int Int | Sun Direction
-
-
-
-initialGrid :: Grid
-initialGrid = Sequence.replicate 20 $ Sequence.replicate 20 $ Tile Drk
-
-test cs = putStrLn $ displayGrid $ foldl (.) id [(addLight (SquareLight 1 3) c)|c <- cs] initialGrid
-
 distance :: Coordinate -> Coordinate -> Int
-distance (x1,y1) (x2,y2) = max (abs $ x1-x2) (abs $ y1-y2) --'Moore distance'
---distance (x1,y1) (x2,y2) = sum [(abs $ x1-x2),(abs $ y1-y2)] --Manhattan distance
---distance (x1,y1) (x2,y2) = floor $ sqrt $ fromIntegral $ sum [(x1-x2)*(x1-x2),(y1-y2)*(y1-y2)] --Euclidean distance
+distance (x1,y1) (x2,y2) = max (abs $ x2-x1) (abs $ y2-y1) --Moore distance
 
-brighten :: Tile -> LightLevel -> Tile
-brighten (Tile tl) ll = if ll > tl
-                        then Tile ll
-                        else Tile tl
+data Light = Light Int -- Bright distance
+                   Int -- Dim distance
 
-addLight :: LightSource -> Coordinate -> Grid -> Grid
-addLight l@(SquareLight r1 r2) (x,y) tss = ((updateTiles (\c -> if r1 > distance (x,y) c then (\t -> brighten t Brt) else id)) . (updateTiles (\c -> if r2 > distance (x,y) c then (\t -> brighten t Dim) else id))) tss
---addLight l@(Sun d) _ tss = 
+data Board = Board Int Int [(Light,Coordinate)]
 
---almost
-{--
-conditionallyBrighten :: Tile -> LightLevel -> (Coordinate -> Bool) -> Tile
-conditionallyBrighten t l f = if f
-                              then brighten t l
-                              else t
---}
+data Brightness = Dark
+                | Dim
+                | Bright
+  deriving (Eq,Ord,Show)
 
-updateTile :: Coordinate -> (Tile -> Tile) -> Seq(Seq Tile) -> Grid
-updateTile (x,y) f tss = update y (adjust f x (index tss y)) tss
+tileBrightness :: Board -> Coordinate -> Brightness
+tileBrightness (Board _ _ ls) c1 = maximum $ map eachLightBrightness ls
+  where eachLightBrightness ((Light brt dim), c2) = case (compare brt $ distance c1 c2, compare dim $ distance c1 c2) of
+                                                           (GT,_ ) -> Bright
+                                                           (EQ,_ ) -> Bright
+                                                           (LT,GT) -> Dim
+                                                           (LT,EQ) -> Dim
+                                                           (LT,LT) -> Dark
+drawTile :: Brightness -> String
+drawTile Dark   = " "
+drawTile Dim    = "▒"
+drawTile Bright = "█"
 
-updateTiles :: (Coordinate -> Tile -> Tile) -> Seq(Seq Tile) -> Grid
-updateTiles f tss = foldl (.) id [(\tss' -> update y (adjust (f (x,y)) x (index tss' y)) tss') | y <- [0..(Sequence.length tss)-1], x <- [0..Sequence.length (index tss y)-1]] $ tss
+drawBoard :: Board -> String
+drawBoard b@(Board width height _) = unlines $ map (\y -> concatMap (drawTile . tileBrightness b) [(x,y)|x <- [0..width]]) [0..height]
 
-
--- displaying the grid as a string for testing purposes
-displayTile :: Tile -> String
-displayTile (Tile l) = displayLightLevel l
-
-displayLightLevel :: LightLevel -> String
-displayLightLevel Brt = "██"
-displayLightLevel Dim = "▒▒"
-displayLightLevel Drk = "░░"
-
-displayGrid :: Grid -> String
-displayGrid grid = foldl (++) "" $ map displayRow $ toList grid
-  where displayRow xs = '\n':(foldl (++) "" $ map displayTile $ toList xs)
+main = putStr $ drawBoard $ Board 15 15 [((Light 2 4),(4,4)), ((Light 2 4),(10,10))]
